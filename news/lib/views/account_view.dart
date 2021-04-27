ac4bce0d11af4a5f5c9b1281a9d1600633aef6ae
AbidahNabilah <abdhnabilah@gmail.com>
2021-04-27 10:23:30 +0700

    Minggu7dan8

---
+++ news/lib/views/account_view.dart
@@ -0,0 +1,14 @@
+import 'dart:html';
+
+import 'package:flutter/material.dart';
+
+class AccountView extends StatelessWidget {
+  @override
+  
+  Widget build(BuildContext context) {
+    return Container(
+      child: Center(
+        child: Text("ACCOUNT VIEW"),
+        ),);
+  }
+}
