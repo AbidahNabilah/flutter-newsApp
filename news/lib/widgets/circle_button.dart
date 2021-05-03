a27890b4bd08713412b81970f8fb0e59b33fec2b
AbidahNabilah <abdhnabilah@gmail.com>
2021-05-03 22:00:51 +0700

    12

--- news/lib/widgets/circle_button.dart
+++ news/lib/widgets/circle_button.dart
@@ -9,26 +9,27 @@ class CircleButton extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return InkWell(
-     
-      borderRadius: BorderRadius.circular(25.0),
-      child: Container(
-        width: 50.0,
-        height: 50.0,
-        decoration: BoxDecoration(
-          shape: BoxShape.circle,
-        ),
-        child: Card(
-          elevation: 1.0,
-          shape: RoundedRectangleBorder(
-            borderRadius: BorderRadius.circular(25.0),
+          onTap: onTap(),
+          borderRadius: BorderRadius.circular(25.0),
+          child: Container(
+            width: 50.0,
+            height: 50.0,
+            decoration: BoxDecoration(
+              shape: BoxShape.circle,
+            ),
+            child: Card(
+              elevation: 1.0,
+              shape: RoundedRectangleBorder(
+                borderRadius: BorderRadius.circular(25.0),
+              ),
+              child: Icon(
+                icon,
+                color: kGrey2,
+                size: 15.0,
+              ),
+            ),
           ),
-          child: Icon(
-            icon,
-            color: kGrey2,
-            size: 15.0,
-          ),
-        ),
-      ),
-    );
+        );
+      
   }
 }
